7cd38b5c4a4791ecc63cd6e467f31b1403ff6aec
Vladimir Novikov <kakifish@hotmail.com>
2013-05-11 09:13:18 +0300

    GREEN: Deal with 900 and above

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -8,37 +8,41 @@ public class RomanNumerals
 
 	public RomanNumerals()
 	{
-		arabicNumerals = new int[6];
-		arabicNumerals[0] = 500;		
-		arabicNumerals[1] = 100;
-		arabicNumerals[2] = 50;
-		arabicNumerals[3] = 10;
-		arabicNumerals[4] = 5;
-		arabicNumerals[5] = 1;
+		arabicNumerals = new int[7];
+		arabicNumerals[0] = 1000;		
+		arabicNumerals[1] = 500;		
+		arabicNumerals[2] = 100;
+		arabicNumerals[3] = 50;
+		arabicNumerals[4] = 10;
+		arabicNumerals[5] = 5;
+		arabicNumerals[6] = 1;
 		
+		specialArabicNumerals = new int[6];
+		specialArabicNumerals[0] = 900;		
+		specialArabicNumerals[1] = 400;		
+		specialArabicNumerals[2] = 90;		
+		specialArabicNumerals[3] = 40;		
+		specialArabicNumerals[4] = 9;
+		specialArabicNumerals[5] = 4;
 		
-		specialArabicNumerals = new int[5];
-		specialArabicNumerals[0] = 400;		
-		specialArabicNumerals[1] = 90;		
-		specialArabicNumerals[2] = 40;		
-		specialArabicNumerals[3] = 9;
-		specialArabicNumerals[4] = 4;
-		
-		romanNumerals = new String[6];
-		romanNumerals[0] = "D";
-		romanNumerals[1] = "C";
-		romanNumerals[2] = "L";
-		romanNumerals[3] = "X";
-		romanNumerals[4] = "V";
-		romanNumerals[5] = "I";
+		romanNumerals = new String[7];
+		romanNumerals[0] = "M";
+		romanNumerals[1] = "D";
+		romanNumerals[2] = "C";
+		romanNumerals[3] = "L";
+		romanNumerals[4] = "X";
+		romanNumerals[5] = "V";
+		romanNumerals[6] = "I";
 
-		specialRomanNumerals = new String[5];
-		specialRomanNumerals[0] = "CD";
-		specialRomanNumerals[1] = "XC";
-		specialRomanNumerals[2] = "XL";
-		specialRomanNumerals[3] = "IX";
-		specialRomanNumerals[4] = "IV";
+		specialRomanNumerals = new String[6];
+		specialRomanNumerals[0] = "CM";
+		specialRomanNumerals[1] = "CD";
+		specialRomanNumerals[2] = "XC";
+		specialRomanNumerals[3] = "XL";
+		specialRomanNumerals[4] = "IX";
+		specialRomanNumerals[5] = "IV";
 
+		
 		
 	}
 	
@@ -52,7 +56,7 @@ public class RomanNumerals
 				numberToRoman += romanNumerals[i];
 				num -= arabicNumerals[i];
 			}
-			if(i != 5 && num >= specialArabicNumerals[i])
+			if(i != 6 && num >= specialArabicNumerals[i])
 			{
 				numberToRoman += specialRomanNumerals[i];
 				num -= specialArabicNumerals[i];
