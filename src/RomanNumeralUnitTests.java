6e02cfbee13b7e848d56c72196907b64208226cf
Vladimir Novikov <kakifish@hotmail.com>
2013-05-11 08:41:45 +0300

    Refactor: methods name changed

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -8,7 +8,7 @@ public class RomanNumeralUnitTests
 	private RomanNumerals arabicNumeral = new RomanNumerals();
 
 	@Test
-	public void if1to3Tests() 
+	public void if1To3Tests() 
 	{
 		assertEquals("I", arabicNumeral.toRoman(1));
 		assertEquals("II", arabicNumeral.toRoman(2));
@@ -16,7 +16,7 @@ public class RomanNumeralUnitTests
 	}
 	
 	@Test
-	public void if4Test()
+	public void if4To8Tests()
 	{
 		assertEquals("IV", arabicNumeral.toRoman(4));
 		assertEquals("V", arabicNumeral.toRoman(5));
